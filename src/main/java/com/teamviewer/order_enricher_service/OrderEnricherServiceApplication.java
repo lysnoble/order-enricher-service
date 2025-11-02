b5636a1009563c1fcd6d5af2f49b5abb725c36a5
lysno <lysnoble@gmail.com>
2025-11-02 13:55:12 -0600

    init

---
+++ src/main/java/com/teamviewer/order_enricher_service/OrderEnricherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamviewer.order_enricher_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEnricherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEnricherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamviewer/order_enricher_service/OrderEnricherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamviewer.order_enricher_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEnricherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
